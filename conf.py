24b0d2e2ebd6e2cf8021c197681254e4ca1c0404
DontCare4Free <webmaster@dontcare4free.tk>
2011-05-29 00:50:09 +0200

    Changed the title to "Blockland"

--- conf.py
+++ conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Blockland Docs'
+project = u'Blockland'
 copyright = u'2011, Blockland community'
 
 # The version info for the project you're documenting, acts as replacement for
